baaca3c584d8b3d3b0d7abbb30d783484887b543
SecanKresimir <secan.kresimir@gmail.com>
2024-11-08 16:25:47 +0100

    cherry-pick

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,8 +4,13 @@
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine(" Drugi pozdrav iz aplikacije Krešimir Sečan , 12413564!");
         }
        
+=======
+            Console.WriteLine(" Pozrdrav iz aplikacije Krešimir Sečan!");
+        }
+>>>>>>> 2bfab38 (first commit conapp)
     }
 }
